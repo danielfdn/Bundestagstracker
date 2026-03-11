bb7892ccb7f68cff7b74e4727f60bc6d427e8afc
Daniel Fandin <danielfandin28@gmail.com>
2026-03-11 12:12:00 +0100

    Inital commit - project structure
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/daniel/bundestagstracker/BundestagstrackerApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.bundestagstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundestagstrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BundestagstrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/bundestagstracker/BundestagstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.bundestagstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundestagstrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
